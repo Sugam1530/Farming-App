e59ed37390d5cb698385b3985285af153202fb00
sugamdas1530 <sugamdas1530@gmail.com>
2022-05-26 19:43:57 +0530

    many changes

---
+++ app/src/main/java/com/example/farming/cartView.java
@@ -0,0 +1,14 @@
+package com.example.farming;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cartView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart_view);
+    }
+}
